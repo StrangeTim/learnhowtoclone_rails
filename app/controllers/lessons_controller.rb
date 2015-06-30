82098f9c6abe2499580d9c2e15d39f62a096d3f3
Steve Bussert & Leon Z <student@epicodus.com>
2015-06-30 11:05:51 -0700

    add CRUD functionality to Lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -17,6 +17,29 @@ class LessonsController < ApplicationController
     end
   end
 
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(lesson_params)
+      redirect_to lessons_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @lesson = Lesson.find(params[:id])
+    @lesson.destroy
+    redirect_to lessons_path
+  end
+
   private
   def lesson_params
     params.require(:lesson).permit(:name)
--- spec/features/lessons_integration_spec.rb
+++ spec/features/lessons_integration_spec.rb
@@ -15,3 +15,27 @@ describe 'the add lesson path' do
     expect(page).to have_content 'errors'
   end
 end
+
+describe 'the show lesson path' do
+  it 'displays the lesson on the show page' do
+    visit lessons_path
+    click_on 'Add a Lesson'
+    fill_in 'Name', :with => "Ruby"
+    click_on "Create Lesson"
+    click_on "Ruby"
+    expect(page).to have_content 'Ruby Details'
+  end
+
+  it 'edits the name of a lesson' do
+    visit lessons_path
+    click_on 'Add a Lesson'
+    fill_in 'Name', :with => "Ruby"
+    click_on "Create Lesson"
+    click_on "Ruby"
+    click_on "Edit Ruby"
+    fill_in 'Name', :with => "FUN!"
+    click_on 'Update Lesson'
+    click_on 'FUN'
+    expect(page).to have_content 'FUN! Details'
+  end
+end
